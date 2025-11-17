257d152f84ba74348b5c7b893386dc677acd158e
Kevin42127 <tyouxipindao@gmail.com>
2025-11-17 14:29:53 +0800

    Fix serverless imports for Vercel

--- api/posts.ts
+++ api/posts.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { sql, initDatabase } from './db'
+import { sql, initDatabase } from './db.js'
 
 export default async function handler(
   req: VercelRequest,
